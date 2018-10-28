96451f7238c3a7d42b480537372f0f72e5249c5d
Vladislav <vit3@rutgers.edu>
2018-10-28 16:54:44 -0400

    finished action handlers and combined reducers

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,1 +1,127 @@
 import axios from 'axios';
+
+let authAxios = axios.create();
+
+authAxios.interceptors.request.use(config => {
+    const token = localStorage.getItem("token");
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+});
+
+const init = dispatch => {
+    dispatch({
+        type: "INIT"
+    });
+};
+
+export const verify = () => {
+    return dispatch => {
+        if(localStorage.getItem("token")){
+            init(dispatch);
+            dispatch({ type:"VERIFY" });
+        };
+    };
+};
+
+export const login = ({ username, password }) => {
+    if(!username || !password) 
+        return {
+            type: "AUTH_ERROR",
+            data: "Username and password required."
+        };
+    return dispatch => {
+        axios.post("/auth/login", { username, password})
+            .then(res => {
+                dispatch({
+                    type: "LOGIN_SUCCESSFUL",
+                    data: res.data
+                });
+            })
+            .then(init(dispatch))
+            .catch(err => console.log(err));
+    };
+};
+
+export const signup = ({ username, password }) => {
+    if(!username || !password) {
+        return {
+            type: "SIGNUP_ERROR",
+            data: "Username and password required."
+        };
+    };
+    return dispatch => {
+        axios.post("/auth/signup", { username, password })
+            .then(res => {
+                init(dispatch);
+                dispatch({
+                    type: "LOGIN_SUCCESSFUL",
+                    data: res.data
+                });
+            })
+            .catch(err => console.log(err));
+    };
+};
+
+export const logout = () => {
+    return {
+        type: "LOGOUT_SUCCESSFUL"
+    };
+};
+
+export const loadEvents = () => {
+    return dispatch => {
+        axios.get('/events')
+            .then(res => {
+                dispatch({
+                    type: "LOAD_EVENTS",
+                    data: res.data
+                });
+            })
+            .catch(err => console.log(err));
+    };
+};
+
+export const selectedEvent = id => {
+    return dispatch => 
+        axios.get(`/events/${id}`)
+            .then(res => 
+                dispatch({
+                    type: "EVENT_SELECTED",
+                    data: res.data
+                }))
+            .catch(err => console.log(err))
+};
+
+export const addEvent = obj => {
+    return dispatch => 
+        authAxios.post("/api/events", obj)
+            .then(res => 
+                dispatch({
+                    type: "EVENT_ADDED",
+                    data: res.data
+                }))
+            .catch(err => console.log(err));
+};
+
+export const deleteEvent = id => {
+    return dispatch => 
+        authAxios.delete(`/api/events/${id}`)
+            .then(res => 
+                dispatch({
+                    type: "EVENT_DELETED",
+                    id
+                }))
+            .catch(err => console.log(err));
+};
+
+export const editEvent = (id, obj) => {
+    return dispatch => 
+        authAxios.put(`/api/events/${id}`, obj)
+            .then(res => 
+                dispatch({
+                    type: "EVENT_UPDATED",
+                    data: res.data
+                }))
+            .catch(err => console.log(err));
+};
+
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -31,9 +31,52 @@ const token = (prev = null, action) => {
     }
 };
 
+const userProfile = (prev = {}, action) => {
+    switch(action.type) {
+        case "VERIFY":
+            return localStorage.getItem("user");
+        case "LOGIN_SUCCESSFUL":
+            return action.data.user;
+        case "LOGOUT_SUCCESSFUL":
+            return {};
+        default:
+            return prev;
+    }
+}
+
+const loadEvents = (prev = [], action) => {
+    switch(action.type) {
+        case "LOAD_EVENTS":
+            return action.data;
+        case "EVENT_ADDED":
+            return [...prev, action.data];
+        case "EVENT_DELETED":
+            return prev.filter(event => event._id !== action.id);
+        case "EVENT_UPDATED":
+            return prev.map(event => {
+                if(event._id === action.data._id) return action.data;
+                return event;
+            });
+        default:
+            return prev;
+    }
+};
+
+const selectedEvent = (prev = null, action) => {
+    switch(action.type) {
+        case "EVENT_SELECTED":
+            return action.data;
+        default:
+            return prev
+    }
+}
+
 const reducer = combineReducers({
     isAuthenticated,
-    token
+    token,
+    userProfile,
+    loadEvents,
+    selectedEvent
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
